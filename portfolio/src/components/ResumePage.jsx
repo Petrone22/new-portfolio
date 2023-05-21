bc5aeedd5e6568a5ebafdc4227d83ec22876e078
Petrone22 <the1rashid@gmai.com>
2023-05-21 15:59:47 +0300

    updated pages and navbar

---
+++ portfolio/src/components/ResumePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import resume from "../assets/";
+const ResumePage = () => {
+  return (
+    <div className=" mt-16 h-screen w-full lg:max-w-7xl relative flex flex-col items-center gap-12 text-textClr md:mb-20 ">
+      <h1 className="text-center text-3xl font-bold  text-purple-700">
+        My Resume
+      </h1>
+    </div>
+  );
+};
+
+export default ResumePage;
